f77caed9857e016cca5b4b1a1de2113be068b819
Gosling <popkornuh@gmail.com>
2024-12-10 17:11:39 +0300

    Спринт 7: Сохранение/загрузка таск менеджера из файла

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-import controllers.FileBackedTaskManager;
-import model.Epic;
-import model.Subtask;
-import model.Task;
 
-import java.io.File;
-
-import static controllers.FileBackedTaskManager.loadFromFile;
 
 
 public class Main {
@@ -13,22 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile(new File("save/load file/file.csv"));
-
-//        FileBackedTaskManager manager = new FileBackedTaskManager();
-//        Task task1 = new Task("Задача", "description1");
-//        Epic epic2 = new Epic("Эпик", "description2");
-//        Subtask subtask3 = new Subtask("Подзадача", "description3", 2);
-//        Subtask subtask4 = new Subtask("Подзадача", "description4", 2);
 //
-//        manager.addTask(task1);
-//        manager.addEpic(epic2);
-//        manager.addSubtask(subtask3);
-//        manager.addSubtask(subtask4);
-
-//        System.out.println(manager.getTasks());
-//        System.out.println(manager.getEpics());
-//        System.out.println(manager.getSubtasks());
 
 
 
